b70dcee98b8e9f30d016e6171c1b45b7209525df
Your Name <deckerwill7@gmail.com>
2023-03-22 22:23:55 -0500

    update

--- Transition Probability Matrix/transition_probability_matrix.R
+++ Transition Probability Matrix/transition_probability_matrix.R
@@ -1,8 +1,48 @@
-# Structured Transition Probability Matrix
+## Load libraries
 
-# Transition matrix
-Stransition <- matrix(0.25, nrow = 4, ncol = 4)
-diag(Stransition) <- 1.00
+library(dplyr)
+library(tidyr)
+library(tibble)
+
+
+## Create structured transition matrix
+
+# initialize matrix
+w <- expand_grid(word = 1:4, syl = 1:3) %>% unite(label, word, syl, sep = "_")
+X <- matrix(0, nrow = nrow(w), ncol = nrow(w), dimnames = list(w$label, w$label))
+
+X["1_1", "1_2"] <- 1
+X["1_2", "1_3"] <- 1
+X["1_3", "1_1"] <- .25
+X["1_3", "2_1"] <- .25
+X["1_3", "3_1"] <- .25
+X["1_3", "4_1"] <- .25
+
+X["2_1", "2_2"] <- 1
+X["2_2", "2_3"] <- 1
+X["2_3", "2_1"] <- .25
+X["2_3", "1_1"] <- .25
+X["2_3", "3_1"] <- .25
+X["2_3", "4_1"] <- .25
+
+X["3_1", "3_2"] <- 1
+X["3_2", "3_3"] <- 1
+X["3_3", "3_1"] <- .25
+X["3_3", "1_1"] <- .25
+X["3_3", "2_1"] <- .25
+X["3_3", "4_1"] <- .25
+
+X["4_1", "4_2"] <- 1
+X["4_2", "4_3"] <- 1
+X["4_3", "4_1"] <- .25
+X["4_3", "1_1"] <- .25
+X["4_3", "2_1"] <- .25
+X["4_3", "3_1"] <- .25
+
+X
+
+
+## Structured Transition Probability Matrix
 
 # Simulate sequence
 n <- 32 # number of trials
@@ -20,7 +60,7 @@ ecountS <- table(rand_seq[1:(n - 1)], rand_seq[2:n]) # empirical count of transi
 dmatS <- diag(1 / rowSums(ecountS))
 eprobS <- dmatS %*% ecountS # empirical transitional probabilities
 
-# Un-structured Transition Probability Matrix
+## Un-structured Transition Probability Matrix
 
 # Define transition matrix
 Rtransition <- matrix(0.083, nrow = 4, ncol = 4)
