9260a6ced86294746027ca56c3f19c72f269bb35
Your Name <deckerwill7@gmail.com>
2023-07-03 11:31:55 -0500

    initial commit

---
+++ Paradigm/check_orders.py
@@ -0,0 +1,15 @@
+# import pandas
+import pandas as pd
+
+# load in list
+df = pd.read_csv('random_orders.csv', delimiter=',')
+order1 = list(df['Order 1'])
+order2 = list(df['Order 2'])
+
+# generate emperical TPM
+eTPM = pd.crosstab(pd.Series(order1[:-1]), pd.Series(order1[1:]), normalize=True, dropna=False) # just checks order1
+
+# Reindex columns and rows to include all syllables
+eTPM = eTPM.reindex(order1, columns=order1, fill_value=0.0)
+
+print(eTPM)
