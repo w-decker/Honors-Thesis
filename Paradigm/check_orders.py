59161efe0a3b9bda839ef39c02a62ddc5d3b1b40
Your Name <deckerwill7@gmail.com>
2023-07-04 10:37:51 -0500

    reindex eTPM * random should be good * still figuring out structured --> should triplets be predefined or created using TPM?

--- Paradigm/check_orders.py
+++ Paradigm/check_orders.py
@@ -6,10 +6,11 @@ df = pd.read_csv('structured_orders.csv', delimiter=',')
 order1 = list(df['Order 1'])
 order2 = list(df['Order 2'])
 
-# generate emperical TPM
+# generate empirical TPM
 eTPM = pd.crosstab(pd.Series(order1[:-1]), pd.Series(order1[1:]), normalize=True, dropna=False) # just checks order1
 
-# Reindex columns and rows to include all syllables
+# Reindex eTPM
+unique_values = pd.unique(order1)
 eTPM = eTPM.reindex(order1, columns=order1, fill_value=0.0)
 
 print(eTPM)
--- Paradigm/generate_structured_orders.py
+++ Paradigm/generate_structured_orders.py
@@ -2,36 +2,38 @@
 import numpy as np
 import csv
 
-# list of words
-words = [['pi', 'tu', 'bi'], ['ba', 'du', 'pi'], ['du', 'ti', 'pa'], 
-         ['bi', 'du', 'ti'], ['tu', 'di', 'ba'], ['bu', 'ti', 'pa']]
+# list of syllables
+syllables = ["di", "da", "du", "pi", "pa", "pu", "bi", "ba", "bu", "ti", "tu", "ta"]
 
-# create TPM
-strucTPM = np.full((6, 6), fill_value=(1/5))
-np.fill_diagonal(strucTPM, 0)
-print(strucTPM)
+# create words using TPM
+strucTPM = np.zeros((12, 12))
+strucTPM[0, 1] = 1
+strucTPM[1, 2] = 1
+strucTPM[2, range(0, 12, 3)] = 0.25
+strucTPM[3, 4] = 1
+strucTPM[4, 5] = 1
+strucTPM[5, range(0, 12, 3)] = 0.25
+strucTPM[6, 7] = 1
+strucTPM[7, 8] = 1
+strucTPM[8, range(0, 12, 3)] = 0.25
+strucTPM[9, 10] = 1
+strucTPM[10, 11] = 1
+strucTPM[11, range(0, 12, 3)] = 0.25
 
 # create orders
-numstim = 144
-initstate = np.random.choice(range(6))  # Random initial state
-order = [words[initstate]]
+numstim = 288
+initstate = np.random.choice(range(12))  # Random initial state
+order = [syllables[initstate]]
 current_state = initstate
 
 while len(order) < numstim:
-    next_state = np.random.choice(range(6), p=strucTPM[current_state])
-    if words[next_state] != order[-1]:
-        order.append(words[next_state])
+    next_state = np.random.choice(range(12), p=strucTPM[current_state])
+    if syllables[next_state] != order[-1]:
+        order.append(syllables[next_state])
         current_state = next_state
 
 # print orders and un-embed word list
-order1 = list(np.concatenate(order))
-order2 = list(np.concatenate(order[::-1])) # order 2 = order 1 in reverse
+order1 = order
+order2 = order[::-1] # order 2 = order 1 in reverse
 print(order1)
 print(order2)
-
-# ouptut orders to .csv
-filename = "structured_orders.csv"
-with open(filename, "w", newline="") as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerow(["Order 1", "Order 2"])
-    writer.writerows(zip(order1, order2))
