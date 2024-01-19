4fcb3900a8809c8af47049b34e63282e2e9fcd2d
Your Name <deckerwill7@gmail.com>
2024-01-19 13:22:48 -0600

    init

---
+++ BIDS/make_heuristic_file.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash -i
+
+# get directories
+while getopts b:d: flag
+do
+    case "${flag}" in
+        b) bidsdir=${OPTARG};;
+        d) didir=${OPTARG};;
+    esac
+done
+
+# output
+echo "Your BIDS directory is $bidsdir";
+echo "Your DICOM directory is $didir";
+
+# make heuristic file
+docker --rm --it \
+-v $didir:/data:ro \
+-v $bidsdir:output \
+nipy/heudiconv:latest \
+
+
