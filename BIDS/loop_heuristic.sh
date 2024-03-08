0b6c8ed99ec495b56d62c05aff6039eb8f6b8ee2
w-decker <107500716+w-decker@users.noreply.github.com>
2024-03-07 20:56:51 -0600

    major updates --> CL args --> better dir parseing --> actually works

--- BIDS/loop_heuristic.sh
+++ BIDS/loop_heuristic.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-# locations
-ds_path=</path/to/dataset/>
-bids_path=</path/to/bids/output/>
-hpy=</path/to/heuristic.py/>
-
-# activate conda env
-conda activate bids # see `setup.sh` to create bids env
-
-PATH=$PATH:~/.local/bin
-
-# remove .heudiconv from bids path to avoid error
-rm -r "$bids_path/.heudiconv"
-
-# get subjects
-subs=(SL*) # glob based on naming convention
-
-# loop throguh all subjects
-for sub in $subs
-do
-    heudiconv -s $sub \
-    -d $dcm_path/{subject}/*/*.MRDC.* \
-    -o $bids_path\
-    -f $hpy \
-    -c dcm2niix -b \
-    --overwrite
+# get flags
+while getopts "b:d:h:" flag; do
+    case "${flag}" in
+        b) bids_path=${OPTARG} ;;
+        d) dcm_path=${OPTARG} ;;
+        h) hpy=${OPTARG} ;;
+        *) echo "Invalid option"; exit 1 ;;
+    esac
 done
 
-# change file permissions for fMRIPrep
-find $bidspath/sub-${$subs//_} -exec chmod ug+w {} \;
-find $bids_path/.heudiconv/${pid//_} -exec chmod ug+w {} \;
+# remove .heudiconv from bids path to avoid error
+# rm -r "$bids_path/.heudiconv"
+
+# get subjects
+subs=(${dcm_path}/SL*)
+
+# loop through all subjects
+for sub in "${subs[@]}"; do
+    # execute conversion
+    heudiconv -s "${sub##*/}" \
+    -d $dcm_path/{subject}/*/*.MRDC.* \
+    -o "$bids_path" \
+    -f "$hpy" \
+    -c dcm2niix -b \
+    --overwrite
+
+    # change file permissions for fMRIPrep
+    find $bidspath/sub-${sub##*/} -exec chmod ug+w {} \;
+    find $bids_path/.heudiconv/${sub##*/} -exec chmod ug+w {} \;
+done
