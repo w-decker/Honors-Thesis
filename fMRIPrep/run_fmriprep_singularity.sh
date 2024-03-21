d0b1d6eb4e646041827d5abc99dbcedb0b758fca
Will Decker <deckerwill7@gmail.com>
2024-03-20 20:37:23 -0500

    update to allow for custom FSL license pointer

--- fMRIPrep/run_fmriprep_singularity.sh
+++ fMRIPrep/run_fmriprep_singularity.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # get flags
-while getopts ":b:w:" flag; do
+while getopts ":b:w:f:" flag; do
     case "${flag}" in
-        b) bids_dir=${OPTARG} ;;
-	w) work_dir_parent=${OPTARG};;
+        b) bids_dir=${OPTARG};;
+        w) work_dir_parent=${OPTARG};;
+        f) fs_license=${OPTARG};;
         *) echo "Invalid option"; exit 1 ;;
     esac
 done
@@ -13,15 +14,14 @@ done
 subs=(${bids_dir}/sub-SL*)
 
 for sub in "${subs[@]}"; do
-	singularity run --cleanenv -B /data/jdecke5:/data/jdecke5 $HOME/images/fmriprep.simg \
-		--work-dir $work_dir_parent/workdir \
-		--participant_label "${sub##*/sub-}" \
-		--output-spaces T1w \
-		--level full \
-		--skip-bids-validation \
-		--fs-license-file $FREESURFER_HOME/license.txt \
-		--cifti-output 91k \
-		$bids_dir \
-		$bids_dir/derivatives \
-		participant
+    singularity run --cleanenv -B /data/jdecke5:/data/jdecke5 $HOME/images/fmriprep.simg \
+        $bids_dir $bids_dir/derivatives participant \
+        --work-dir $work_dir_parent \
+        --participant_label "${sub##*/sub-}" \
+        --output-spaces T1w \
+        --level full \
+        --skip-bids-validation \
+        --fs-license-file "${fs_license}" \
+        --cifti-output 91k
+
 done
